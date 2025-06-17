e4e6d5ea710d1b1209baf2d173e8f80457110dc7
jaeminChung <hijam76@gmail.com>
2025-06-17 19:37:21 +0900

    let there be light

--- animation.py
+++ animation.py
@@ -1,9 +1,17 @@
 import pygame
 class animation:
-    def __init__(self,image,width):
+    def __init__(self,image,width,interval):
+        self.interval=interval
+        self.cin=0
+        self.curser=0
         self.images =[]
         for i in range(0,image.get_width()+width,width):
             tmp_image=image.subsurface(pygame.Rect(i, 0, width, image.get_height))
             self.images.append(tmp_image)
+    def next(self):
+        self.curser+=1
+        self.cin=self.curser//self.interval
+    def get_curren_image(self):
+        return self.images[self.cin]
 
 
