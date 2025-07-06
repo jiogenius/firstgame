e0374ff3ea4101f96b2c3a3cc644344576933867
enterike <enterike0917@gmail.com>
2025-07-06 21:55:53 +0900

    d

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-import pygame
-import imageManager
-
-image = imageManager.imagemanager.get_image("asd", 1)
-#
+import random
+seedlist = ['apple','Sugar_apple','Sunflower','Ember_lily']
+class garden:
+    def __init__(self, seed):
+        self.stock = seed
+    def restock(self):
+        print(self.stock)
+    def power_of_robux(self):
+        
+ike = garden(seedlist[random.randint(0,3)])
+ike.restock()
