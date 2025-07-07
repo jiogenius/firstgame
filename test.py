56f69175d788edbd7959bd185e8f46a9a7206db1
jaeminChung <hijam76@gmail.com>
2025-07-07 16:04:51 +0900

    remove unused imports and add a placeholder class definition

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-import setting
-import os
-
-mainFolderPath = os.path.dirname(os.path.abspath(__file__))
-
-a = setting.Setting(mainFolderPath)
-print(a.get("playerSetting")["a"])
+class test:
+    def
